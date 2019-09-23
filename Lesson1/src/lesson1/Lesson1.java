bc3e4469492295badd443c2c8c9d7060699806b7
letai2001 <nguyenletai16122001@gmail.com>
2019-09-23 20:57:21 +0700

    lesson 1

---
+++ Lesson1/src/lesson1/Lesson1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Lesson1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // TODO code application logic here
+         
+         
+         float a = 10;
+         int b = 6;
+         if (a==0&&b==0){
+             System.out.println("vo so nghiem");
+              
+            
+         }
+             if (a==0&&b!=0){
+                 System.out.println("vo nghiem");    
+             }
+             if (a!=0) {
+         float x = -b/a;
+         System.out.println("nghiem la") ;
+         System.out.println(x) ;
+              if(a==0) {  
+                if (b==0)
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+      }
+    
+}
