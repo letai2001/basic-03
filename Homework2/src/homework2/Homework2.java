7e93959377a0eec77576fb5f6188c909bc73727e
letai2001 <nguyenletai16122001@gmail.com>
2019-09-30 15:20:57 +0700

    basic-03

---
+++ Homework2/src/homework2/Homework2.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Homework2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k ;
+        k = sc.nextInt();
+        int count = 0;
+        
+        for (int i = 2; i <= k; i++) {
+            for (int j = 1; j <= j; j++) {
+                if (i % j == 0){
+                   count++ ;
+                 
+                }
+                                
+                
+            }if (count == 2) {
+                    System.out.println(i);
+                    count = 0;  
+            
+        }
+            
+                
+                   
+            
+                
+                    
+                
+            
+            
+
+        
+
+    }
+
+
+    
+
